885bbd3f462c208f0c4cd4d9137ca7aa92a94ef7
musabmahmud <md.musabmahmud100@gmail.com>
2024-08-16 20:52:11 +0600

    products added with filtered option

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Card = ({ product }) => {
+  return (
+    <div className='shadow-sm rounded overflow-hidden'>
+      <Link to={`shop/${product.id}`}>
+        <img className='hover:scale-110 transition-all duration-300' src={product.image} alt={product.title} />
+      </Link>
+      <div className='py-4 px-4 font-open-sans'>
+        <h4 className="text-base open-sans font-semibold mb-2 ">{product.title}</h4>
+        <div className="flex justify-between items-center">
+          <p className='text-black-default'>{product.category}</p>
+          <p className='font-semibold'>${product.price}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/pages/home/Products.jsx
+++ src/pages/home/Products.jsx
@@ -1,53 +1,108 @@
 import React, { useEffect, useState } from 'react'
 import { FaFilter } from 'react-icons/fa'
+import Cards from '../../components/Card';
+import Card from '../../components/Card';
 
 const Products = () => {
 
     const [products, setProducts] = useState([]);
+    const [filteredItems, setFilteredItems] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("all");
+    const [sortOption, setSortOption] = useState("default");
 
-    useEffect(()=>{
-        const fetchData = async () =>{
-            try{
+    // fetching data from json 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
                 const response = await fetch("./products.json");
                 const data = await response.json();
                 setProducts(data);
+                setFilteredItems(data);
 
-            } catch(error){
-                console.log(error);
+            } catch (error) {
+                console.log("Error Fetching Data", error);
             }
         }
 
         fetchData();
     }, [])
 
-    console.log(products);
+
+    //Filtering Category
+
+    const filterItems = (category) => {
+        const filtered = category === "all" ? products : products.filter((item) => item.category === category);
+        setFilteredItems(filtered);
+        setSelectedCategory(category);
+    }
+
+    //Sort Products
+
+    const showAll = () => {
+        setFilteredItems(products);
+        selectedCategory("all");
+    }
+
+
+    // Sorting Functionality 
+
+    const handleSortChange = (option) => {
+
+        setSortOption(option);
+        // logic for sorting 
+        let sortedItems = [...filteredItems];
+
+        switch (option) {
+            case "a-z":
+                sortedItems.sort((a, b) => a.title.localeCompare(b.title));
+                break;
+            case "z-a": sortedItems.sort((a, b) => b.title.localeCompare(a.title));
+                break;
+            case "low-to-high": sortedItems.sort((a, b) => a.price - b.price);
+                break;
+            case "high-to-low": sortedItems.sort((a, b) => b.price - a.price);
+                break;
+            default: sortedItems.sort();
+        }
+
+        setFilteredItems(sortedItems);
+    }
+
+
     return (
         <section>
             <div className='max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12'>
                 <div>
                     <h2 className='title'>Or subscribe to the newsletter</h2>
                 </div>
-                <div>
-                    <div className='flex flex-col md:flex-row flex-wrap justify-between items-center space-y-3 mb-8'>
-                        <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
-                            <button>All Products</button>
-                            <button>Clothing</button>
-                            <button>Hoodies</button>
-                            <button>Bag</button>
-                        </div>
-                        <div className='flex justify-end mb-4 rounded-sm'>
-                            <div className='bg-black p-2'>
-                                <FaFilter className='text-white h-4 w-4' />
-                            </div>
-                            <select className='bg-black text-white p-1 rounded-sm'>
-                                <option value="default">Filter</option>
-                                <option value="a-z">A-Z</option>
-                                <option value="z-a">Z-A</option>
-                                <option value="low-to-high">Low to High</option>
-                                <option value="high-to-low">High to Low</option>
-                            </select>
-                        </div>
+                <div className='flex flex-col md:flex-row flex-wrap justify-between items-center space-y-3 mb-8'>
+                    <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
+                        <button onClick={showAll}>All Products</button>
+                        <button onClick={() => filterItems("Dress")}>Clothing</button>
+                        <button onClick={() => filterItems("Hoodies")}>Hoodies</button>
+                        <button onClick={() => filterItems("Bag")}>Bag</button>
+                        <button onClick={() => filterItems("Glasses")}>Glasses</button>
+                        <button onClick={() => filterItems("Shoe")}>Shoe</button>
                     </div>
+                    <div className='flex justify-end mb-4 rounded-sm'>
+                        <div className='bg-black p-2'>
+                            <FaFilter className='text-white h-4 w-4' />
+                        </div>
+                        <select id='sort' onChange={(e) => handleSortChange(e.target.value)} value={sortOption} className='bg-black text-white p-1 rounded-sm'>
+                            <option value="default">Filter</option>
+                            <option value="a-z">A-Z</option>
+                            <option value="z-a">Z-A</option>
+                            <option value="low-to-high">Low to High</option>
+                            <option value="high-to-low">High to Low</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 xl:gap-12'>
+                    {
+                        filteredItems.slice(0, 8).map((product) => (
+                            <Card key={product.id} product={product} />
+                        ))
+                    }
                 </div>
             </div>
         </section>
