8c3e9371675fadc3f1858e1bafa4d50bf1b70fae
musabmahmud <md.musabmahmud100@gmail.com>
2024-09-24 17:50:28 +0600

    cart All Clear

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -26,9 +26,40 @@ const cartSlice = createSlice({
 
             localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
         },
+        removeFromCart(state, action) {
+            const nextCartItems = state.cartItems.filter(
+                item => item.id !== action.payload.id
+            )
+            state.cartItems = nextCartItems;
+            localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        },
+        cartQuantityIncrement(state, action) {
+            const itemIndex = state.cartItems.findIndex(item => item.id === action.payload.id);
+            if (itemIndex >= 0) {
+                state.cartItems[itemIndex].cartQuantity = action.payload.cartQuantity;
+                localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+            }
+        },
+        cartQuantityDecrement(state, action) {
+            const itemIndex = state.cartItems.findIndex(item => item.id === action.payload.id);
+            if (itemIndex >= 0) {
+                state.cartItems[itemIndex].cartQuantity = action.payload.cartQuantity;
+                localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+            }
+        },
+        clearCart(state, action) {
+            state.cartItems = []
+            localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        },
+        getTotal(state, action) {
+            state.cartItems.reduce(() => { }, {
+                total: 0,
+                quantity: 0, 
+            })
+        }
     }
 })
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, cartQuantityIncrement, cartQuantityDecrement, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,105 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaArrowAltCircleLeft, FaHome } from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { cartQuantityDecrement, cartQuantityIncrement, clearCart, removeFromCart } from '../features/cartSlice'
+import { Bounce, toast } from 'react-toastify'
 
 const Cart = () => {
 
     const cart = useSelector((state) => state.cart);
+
+    const dispatch = useDispatch();
+
+    const handleRemoveFromCart = (cart) => {
+
+        dispatch(removeFromCart(cart));
+
+        toast.error(`
+            Successfully Remove From Cart`, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+            transition: Bounce,
+        });
+
+    }
+
+
+
+    const quantityDecrement = (product) => {
+        const proQuan = Number(product.cartQuantity) - 1;
+
+        if (proQuan >= 1) {
+            const quantity = document.getElementById(`quantity-${product.id}`);
+
+            quantity.value = proQuan;
+
+            product = { ...product, cartQuantity: proQuan };
+
+            dispatch(cartQuantityDecrement(product));
+
+            toast.info(`
+                Successfully Decrement Quantity`, {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                theme: "light",
+                transition: Bounce,
+            });
+        }
+    }
+
+    const quantityIncrement = (product) => {
+
+        const proQuan = Number(product.cartQuantity) + 1;
+
+        const quantity = document.getElementById(`quantity-${product.id}`);
+        quantity.value = proQuan;
+
+        product = { ...product, cartQuantity: proQuan };
+
+        dispatch(cartQuantityIncrement(product));
+
+        toast.success(`
+            Successfully Increment Quantity`, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+            transition: Bounce,
+        });
+
+    }
+
+
+    const clearAllCart = () => {
+
+        dispatch(clearCart());
+
+        toast.success(`
+            Successfully Empty Cart`, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "light",
+            transition: Bounce,
+        });
+    }
+
 
 
     return (
@@ -28,8 +122,8 @@ const Cart = () => {
                         <div className='text-right'>Amount</div>
                     </div>
                     {
-                        cart.cartItems?.map((product, index) => (
-                            <div key={index} className='grid grid-flow-col grid-cols-6 items-center gap-4 p-4 border shadow-sm rounded mb-5'>
+                        cart.cartItems?.map((product) => (
+                            <div key={product.id} className='grid grid-flow-col grid-cols-6 items-center gap-4 p-4 border shadow-sm rounded mb-5'>
                                 <div>
                                     <img src={product.image} alt={product.title} className='rounded' width="100" height="100" />
                                 </div>
@@ -37,14 +131,17 @@ const Cart = () => {
                                     <h4 className="open-sans font-semibold mb-4 text-xl">{product.title}</h4>
                                     <p>Category: <span className='text-gray-500'></span>{product.category}</p>
                                     <p>Price: <span className='font-semibold'>${product.price}</span></p>
+                                    <button className='text-slate-500 hover:text-red-600 transition-default' onClick={() => handleRemoveFromCart(product)}>Remove</button>
                                 </div>
                                 <div className='flex items-center justify-center font-open-sans'>
-                                    <button className='pr-2'>-</button>
-                                    <input className='text-center p-2 border w-full' type="text" name="" value={product.cartQuantity} id="" />
-                                    <button className='pl-2'>+</button>
+                                    <button onClick={() => quantityDecrement(product)} className='pr-2'>-</button>
+
+                                    <input className='text-center border w-full lg:p-2' id={`quantity-${product.id}`} min={0} type="text" name="" defaultValue={product.cartQuantity} />
+
+                                    <button onClick={() => quantityIncrement(product)} className='pl-2'>+</button>
                                 </div>
                                 <div className='text-right'>
-                                    <span className=" font-semibold">${product.price * product.cartQuantity}</span>
+                                    <span className=" font-semibold">${Math.round(product.price * product.cartQuantity)}</span>
                                 </div>
                             </div>
                         ))
@@ -52,16 +149,15 @@ const Cart = () => {
                 </div>
                 <div className="lg:w-1/4 mt-10 lg:mt-0">
                     <div className='mb-5'>
-                        <button className='bg-red-500 px-4 py-2 rounded text-white w-full'>Clear Cart</button>
+                        <button onClick={clearAllCart} className='bg-red-500 px-4 py-2 rounded text-white w-full'>Clear Cart</button>
                     </div>
                     <div className="flex justify-between border shadow-sm rounded bg-slate-50 p-4">
                         <span>Subtotal:  </span><span className='font-bold'>$999</span>
                     </div>
-                    <button className='mt-5 px-4 py-2 rounded w-full flex justify-end items-center gap-2'><FaArrowAltCircleLeft /> Continue to Shopping</button>
+                    <Link to='/shop' className='mt-5 px-4 py-2 rounded w-full flex justify-end items-center gap-2'><FaArrowAltCircleLeft /> Continue to Shopping</Link>
                 </div>
             </div>
         </section>
-
     )
 }
 
